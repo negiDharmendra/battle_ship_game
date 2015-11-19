65bc823c833edc22610440ecf3d2b62f6f687cab
Vikas K U <vikasku@thoughtworks.com>
2015-11-19 22:28:58 +0530

    test passing for player constructor

--- server/battleship.js
+++ server/battleship.js
@@ -1,12 +1,29 @@
+"use strict"
 var sh = {};
 exports.sh = sh;
+var ld = require('lodash');
 
 sh.Ship = function(name,holes){
-	var ship = this;
-	ship.name =  name;
-	ship.holes = holes;
-	Object.defineProperties(ship,{
+	this.name =  name;
+	this.holes = holes;
+	Object.defineProperties(this,{
 		name:{writable:false},
 		holes:{writable:false}
 	});
+};
+
+sh.Player = function(player_name){
+	this.name = player_name;
+	var holes = [4,5,3,2,3];
+	this.fleet =[ 'battleship', 'carrier', 'cruiser', 'distroyer', 'submarine' ].map(function(name,i){
+		return new sh.Ship(name,holes[i]);
+	})
+};
+
+sh.Player.prototype = {
+	validatePosition : function(shipName,position){
+		return position.every(function(pos){
+			return ld.inRange(parseInt(pos.slice(1)),1,11) && ld.inRange(pos[0].charCodeAt(),65,75);
+		});
+	}
 };
