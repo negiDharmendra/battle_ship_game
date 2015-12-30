d3cbbf6c63b7c21d17a1c5958d47c16af5339fe0
Bindu S <bindus@thoughtworks.com>
2015-12-30 19:29:27 +0530

    [Abhishek-Bindu] - Created Games constructor.
    What changes have you made?
    
    We have created Games and various behaviors associated to it.
    
    Why do we need this change?
    
    We are planning our game to be multipair game.For that we need games object
    to handle all games associated with it.

---
+++ library/games.js
@@ -0,0 +1,56 @@
+var ld = require('lodash');
+
+var Games = function (Game) {
+	this.allGames = {};
+	this.createGame = function(player){
+		player.playerId = this.getPlayerId();
+		var game = new Game(player);
+		game.gameId = this.getGameId();
+		this.allGames[game.gameId]=game;
+		return game;
+	},
+	this.joinGame = function(game,player){
+		player.playerId = this.getPlayerId();
+		return game.addPlayer(player);
+	}
+}
+
+
+Games.prototype = {
+	getGameId : (function(){
+		var id = 100;
+		return function(){return id++;};
+	})(),
+	getPlayerId : (function(){
+		var id = 1;
+		return function(){return id++;};
+	})(),
+	getGame : function(gameId){
+		var game = this.allGames[gameId];
+		if(game) return game;
+		return null;
+	},
+	getInitializedGames : function(){
+		var games = this.allGames;
+		var initializedGames = {};
+		for(var game in games){
+			game = this.getGame(game);
+			if(game.status()=="initialized"){
+				initializedGames[game.gameId]=game;
+			}
+		}
+		return initializedGames;
+	},
+	deleteGame : function(gameId){
+		delete this.allGames[gameId];
+	},
+	ensureValidGame : function(gameId){
+		return !!this.getGame(gameId);
+	},
+	restartGame : function(game){
+		//pending
+	}
+}
+
+
+module.exports = Games;
---
+++ test/gamesTest.js
@@ -0,0 +1,68 @@
+var Games = require('../library/games.js');
+var chai = require('chai');
+var sinon = require('sinon');
+
+
+describe('Games',function () {
+	var Game;
+	var games;
+	beforeEach(function(){
+		Game = sinon.spy();
+		games = new Games(Game);
+	});
+	it('can create a new game',function(){
+		var player = {};
+		games.createGame(player);
+		var noOfGames = Object.keys(games.allGames).length;
+		chai.expect(noOfGames).to.be.equal(1);
+		chai.expect(Game.calledOnce).to.be.true;
+	});
+	it('can join a game',function(){
+		var game = {addPlayer:sinon.spy()};
+		var player = {};
+		games.joinGame(game,player);
+		chai.expect(game.addPlayer.calledWith(player));
+	});
+	it('can find a game',function(){
+		var player = {};
+		var newgame = games.createGame(player);
+		var game = games.getGame(newgame.gameId);
+		chai.expect(game).to.be.equal(newgame);
+	});
+	it('gives all the games which can be joined',function(){
+		var Game = function(){this.status=function(){
+			return 'initialized';
+		}};
+		var games = new Games(Game);
+		var player = {};
+		var newgame = games.createGame(player);
+		var existinggames = games.getInitializedGames();
+		chai.expect(Object.keys(existinggames).length).to.be.equal(1);
+	});
+	it('gives null if there is no game that can be joined',function(){
+		var Game = function(){this.status=function(){
+			return 'initialized';
+		}};
+		var games = new Games(Game);
+		var player = {};
+		var existinggames = games.getInitializedGames();
+		chai.expect(Object.keys(existinggames).length).to.be.equal(0);
+	});
+	it('delete a game',function(){
+		var player = {};
+		var newgame = games.createGame(player);
+		games.deleteGame(newgame.gameId);
+		var allGames = games.allGames;
+		chai.expect(Object.keys(allGames).length).to.be.equal(0);
+	});
+	it('will ensure a that a game is valid',function(){
+		var player = {};
+		var newgame = games.createGame(player);
+		chai.expect(games.ensureValidGame(newgame.gameId)).to.be.true;
+	});
+	it('will ensure a that a game is not valid',function(){
+		var player = {};
+		var randomGame = {gameId:434};
+		chai.expect(games.ensureValidGame()).to.be.false;
+	});
+});
